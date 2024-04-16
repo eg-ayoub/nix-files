1b67fc4a7293ff8a5a3fea19d0504b08a255f747
Ayoub Nasr <ayoub.nasr@scality.com>
2024-04-16 12:14:36 +0200

    mouse flake

---
+++ hosts/mouse/home.nix
@@ -0,0 +1,31 @@
+{ ... } : {
+  
+  imports = [
+    ./../../modules
+  ];
+
+  home.username = "ayoub";
+  home.homeDirectory = "/home/ayoub";
+
+  home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
+
+  shell.zsh.enable = true;
+  shell.starship.enable = true;
+
+  editor.nvim.enable = true;
+  editor.servers.enable = true;
+
+  # hyprland.hyprland.enable = true;
+
+  kitty.enable = true;
+
+  apps.chrome.enable = true;
+  apps.slack.enable = true;
+  apps.logseq.enable = true;
+  apps.spotify.enable = true;
+
+  git.enable = true;
+  git.email = "ayoub.nasr@scality.com";
+  git.enable-signing = true;
+}
