9debb4563cb23f7665b789549d8b297ab414ea09
Ayoub Nasr <ayoub.nasr@scality.com>
2024-08-29 15:58:07 +0200

    no nvidia for mouse :(

--- modules/system/misc.nix
+++ modules/system/misc.nix
@@ -4,8 +4,6 @@
   
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
-
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "23.11";
 }
