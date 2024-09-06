c8cd28b1cb8c2c779f84705cf71bc1e6e2c93f9b
Ayoub Nasr <ayoub.nasr@scality.com>
2024-09-06 11:16:46 +0200

    stop using harpoon

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # nix-files
 My nixos configuration files.
-
-## TODO
-- [x] zsh
-- [x] secret management
-- [-] nvim
-- [-] hyprland
-- [x] kitty
-- [x] browsers
-- [x] extra apps
--- modules/editor/nvim.nix
+++ modules/editor/nvim.nix
@@ -50,21 +50,6 @@ in
           '';
         }
         {
-          plugin = harpoon;
-          config = ''
-          lua << EOF
-          vim.g.mapleader = " "
-          local mark = require("harpoon.mark")
-          -- local ui = require("harpoon.ui")
-          vim.keymap.set("n", "<leader>a", mark.add_file)
-          -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-          -- vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-          -- vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-          -- vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-          EOF
-          '';
-        }
-        {
           plugin = kanagawa-nvim;
           config = ''
           lua << EOF
@@ -107,8 +92,6 @@ in
           vim.keymap.set('n', '<C-p>', ts_builtin.git_files, {})
           vim.keymap.set('n', '<leader>ps', ts_builtin.live_grep)
           vim.keymap.set('n', '<leader>vh', ts_builtin.help_tags, {})
-          vim.keymap.set("n", "<C-e>", "<cmd>:Telescope harpoon marks<CR>")
-          require("telescope").load_extension('harpoon')
           EOF
           '';
         }
