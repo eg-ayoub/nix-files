4335ff1e7d7f49a173732d3ac431e152cb5723d1
Ayoub Nasr <ayoub.nasr@proton.me>
2024-09-07 18:33:02 +0200

    fix gpg conf

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -34,6 +34,7 @@ in
       export GPG_TTY="$(tty)"
       export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
       gpgconf --launch gpg-agent
+      gpg-connect-agent updatestartuptty /bye > /dev/null
       '';
     };
   };
