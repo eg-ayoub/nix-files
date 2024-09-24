5f777f8c36f6452320efa223e73ef5fdb9ba867f
Ayoub Nasr <ayoub.nasr@scality.com>
2024-09-24 16:26:52 +0200

    walls: update and add more

--- pkgs/mywalls.nix
+++ pkgs/mywalls.nix
@@ -2,29 +2,32 @@
 {
   mywalls = stdenv.mkDerivation rec {
     pname = "mywalls";
-    version = "0.1";
+    version = "0.2";
     dontBuild = true;
     installPhase = ''
       mkdir -p $out/aesthetic $out/nixos
       cp -a aesthetic/images/* $out/aesthetic
       cp -a nixos/wallpapers/* $out/nixos
       find $out -name "*arch*" -exec rm {} \;
+      find $out -name "*ubuntu*" -exec rm {} \;
+      find $out -name "*debian*" -exec rm {} \;
+      find $out -name "*windows*" -exec rm {} \;
     '';
     sourceRoot = ".";
     srcs = [
       (fetchFromGitHub {
         owner = "D3Ext";
         repo = "aesthetic-wallpapers";
-        rev = "060c580dcc11afea2f77f9073bd8710920e176d8";
+        rev = "b87620b9188a512fe5c81a4688fc1bca1bfb4a41";
         name = "aesthetic";
-        sha256 = "sha256-5MnW630EwjKOeOCIAJdSFW0fcSSY4xmfuW/w7WyIovI=";
+        sha256 = "sha256-NpNjtMYQNZw1rpOVTOdltAdbh5LxHO786huLkW63yQ0=";
       })
       (fetchFromGitHub {
         owner = "NixOS";
         repo = "nixos-artwork";
-        rev = "f38303238fb03b89e29b6c8c2fd32e59df59a2d5";
+        rev = "de03e887f03037e7e781a678b57fdae603c9ca20";
         name = "nixos";
-        sha256 = "sha256-4EiAn8n04aHcHA/DdCbwkagsSxdzADrn0CwA9ZXo+7c=";
+        sha256 = "sha256-78FyNyGtDZogJUWcCT6A/T2MK87nGN/muC7ANH1b1V8=";
       })
     ];
   };
