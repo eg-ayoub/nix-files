4c1d3bd5482b2dd4e662f60ea0711cdc736a60e8
Ayoub Nasr <ayoub.nasr@scality.com>
2025-07-08 15:39:42 +0200

    Add git signs to oil nvim

--- dotfiles/nvim/dot-config/nvim/lua/modules/oil.lua
+++ dotfiles/nvim/dot-config/nvim/lua/modules/oil.lua
@@ -1,5 +1,9 @@
+require("oil-git-status").setup()
 require("oil").setup({
   view_options = {
     show_hidden = true,
+  },
+  win_options = {
+    signcolumn = "yes:2",
   },
 })
--- dotfiles/nvim/dot-config/nvim/lua/modules/plugins/oil.lua
+++ dotfiles/nvim/dot-config/nvim/lua/modules/plugins/oil.lua
@@ -1,6 +1,11 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy = false,
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+  },
+  {
+    "refractalize/oil-git-status.nvim",
+  }
 }
