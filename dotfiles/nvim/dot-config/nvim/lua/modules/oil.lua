599e9b86068a226f497cb61eef97713ec39dcf65
Ayoub Nasr <ayoub.nasr@proton.me>
2026-04-29 17:56:17 +0200

    oil nvim: ignore uid files, telescope: ignore uid, images, import

--- dotfiles/nvim/dot-config/nvim/lua/modules/oil.lua
+++ dotfiles/nvim/dot-config/nvim/lua/modules/oil.lua
@@ -1,6 +1,9 @@
 require("oil").setup({
   view_options = {
-    show_hidden = true,
+    show_hidden = false,
+    is_hidden_file = function (name, _)
+      return vim.startswith(name, ".") or vim.endswith(name, ".uid")
+    end
   },
   win_options = {
     signcolumn = "yes:2",
--- dotfiles/nvim/dot-config/nvim/lua/modules/telescope.lua
+++ dotfiles/nvim/dot-config/nvim/lua/modules/telescope.lua
@@ -1,6 +1,13 @@
 local telescope = require('telescope')
 telescope.load_extension('git_branch')
 telescope.setup {
+  defaults = {
+    file_ignore_patterns = {
+      "%.uid",
+      "%.png %.jpg %.svg",
+      "%.import",
+    },
+  },
   pickers = {
     find_files = {
       find_command = { 'rg', '--files', '--hidden', '--follow', '-g', '!.git' },
