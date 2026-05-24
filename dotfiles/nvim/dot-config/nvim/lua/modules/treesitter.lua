38efd65ddf70a84fc4053dc827538d24b81259f6
Ayoub Nasr <ayoub.nasr@proton.me>
2026-05-24 15:25:51 +0200

    fix lsp, auto highligh all file types

--- dotfiles/nvim/dot-config/nvim/lua/modules/treesitter.lua
+++ dotfiles/nvim/dot-config/nvim/lua/modules/treesitter.lua
@@ -1,1 +1,11 @@
-require('nvim-treesitter').install { 'go', 'python', 'lua', 'markdown', 'vim', 'yaml', 'cpp', 'gdscript', 'nix' }
+require('nvim-treesitter').install {
+  'go', 'python', 'lua', 'markdown', 'vim', 'yaml', 'cpp', 'gdscript', 'nix',
+  'bash'
+}
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    pcall(vim.treesitter.start)
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
