38b4c5db050913ffa9002f2dc586b7d55da4b043
Ayoub Nasr <ayoub.nasr@proton.me>
2026-08-13 15:23:04 +0200

    feat: add host/port config to llama-server

--- modules/services/llama-server.nix
+++ modules/services/llama-server.nix
@@ -1,20 +1,41 @@
 { ... }:
 {
   flake.nixosModules.llama-server =
-    { pkgs, ... }:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
     let
+      cfg = config.svc.llama-server;
       models-dir = "/var/lib/llama-models";
     in
     {
+      options.svc.llama-server = {
+        host = lib.mkOption {
+          type = lib.types.str;
+          default = "0.0.0.0";
+          description = "address to bind llama-server to";
+        };
+        port = lib.mkOption {
+          type = lib.types.port;
+          default = 8080;
+          description = "port on which to serve llama-server";
+        };
+      };
       config = {
         services.llama-cpp = {
           enable = true;
           package = pkgs.llama-cpp-vulkan;
           settings = {
-            sleep-idle-seconds = 3600;
+            host = cfg.host;
+            port = cfg.port;
+            sleep-idle-seconds = 1800;
             models-max = 2;
             models-preset = "${models-dir}/presets.ini";
           };
+          openFirewall = true;
         };
         systemd.services.llama-cpp = {
           environment = {
